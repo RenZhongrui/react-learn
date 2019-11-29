6329698715fe561aaf25dd84fa6c907b3cd1d3b2
renzhongrui <zrzoffice@163.com>
2019-11-29 13:59:05 +0800

    渲染虚拟dom

--- 001-react-virtual-dom-diff/src/Element.js
+++ 001-react-virtual-dom-diff/src/Element.js
@@ -15,9 +15,41 @@ function createElement(type, props, children) {
     return new Element(type, props, children);
 }
 
+function setAttr(node, key, value) {
+    switch (key) {
+        case "value": // input 或者 textarea
+            if (node.tagName.toUpperCase() === "INPUT" || node.tagName.toUpperCase() === "TEXTAREA") {
+                node.value = value;
+            } else {
+                node.setAttribute(key, value);
+            }
+            break;
+        case "style":
+            node.style.cssText = value;
+            break;
+        default:
+            node.setAttribute(key, value);
+            break;
+    }
+}
 // render方法将虚拟DOM转化为真实DOM
 function render(virtualDom) {
-    
+    // 创建真实dom元素
+    let el = document.createElement(virtualDom.type);
+    // 解析dom元素的属性
+    for (let key in virtualDom.props) {
+        setAttr(el,key,virtualDom.props[key]);
+    }
+    virtualDom.children.forEach(child => {
+        // 判断子元素是不是虚拟dom，如果是就接着渲染，如果不是就是文本节点
+        child = (child instanceof Element)? render(child): document.createTextNode(child);
+        el.appendChild(child);
+    });
+    return el;
 }
 
-export {createElement, render};
+function renderDom(el, target) {
+    target.appendChild(el);
+}
+
+export {createElement, render, renderDom, Element};
--- 001-react-virtual-dom-diff/src/index.js
+++ 001-react-virtual-dom-diff/src/index.js
@@ -1,14 +1,20 @@
 /**
  * create: Ren Zhongrui
  * date: 2019-11-29
- * description: npm install -g create-react-app
+ * description:
+ * npm install -g create-react-app
+ * create-react-app appName
  */
-import {createElement, render} from "./Element";
+import {createElement, render, renderDom} from "./Element";
+// 创建虚拟DOM, 虚拟dom是描述DOM信息的对象
 let virtualDom = createElement("ul",{class:'list'},[
     createElement("li",{class:'item'},['a']),
     createElement("li",{class:'item'},['a']),
     createElement("li",{class:'item'},['a'])
 ]);
+// 虚拟DOM转化成了真实DOM
 let el = render(virtualDom);
+// 将DOM渲染到页面
+renderDom(el,window.root);
 console.log(el);
 console.log(virtualDom);
